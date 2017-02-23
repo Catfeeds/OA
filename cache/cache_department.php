8fcd1b9d40dd2a40b66f77bcde072c14f4c00c1d
chinesebear <nuptbear@gmail.com>
2017-02-23 12:00:39 +0800

    上传代码

---
+++ cache/cache_department.php
@@ -0,0 +1,37 @@
+<?php
+//该文件是系统自动生成的缓存文件，请勿修改
+//创建时间：2017-02-23 09:54:57
+
+if (!defined('IN_TOA')) {exit('Access Denied!');}
+
+$_CACHE['department'] = array (
+  1 => 
+  array (
+    'id' => '1',
+    'persno' => '负责人为空?',
+    'name' => '研发部',
+    'date' => '2017-02-23 09:54:12',
+    'father' => '0',
+    'uid' => NULL,
+  ),
+  2 => 
+  array (
+    'id' => '2',
+    'persno' => '负责人为空?',
+    'name' => '市场部',
+    'date' => '2017-02-23 09:54:12',
+    'father' => '0',
+    'uid' => NULL,
+  ),
+  3 => 
+  array (
+    'id' => '3',
+    'persno' => '负责人为空?',
+    'name' => '人事部',
+    'date' => '2017-02-23 09:54:12',
+    'father' => '0',
+    'uid' => NULL,
+  ),
+);
+
+?>
---
+++ cache/cache_position.php
@@ -0,0 +1,34 @@
+<?php
+//该文件是系统自动生成的缓存文件，请勿修改
+//创建时间：2017-02-23 09:57:55
+
+if (!defined('IN_TOA')) {exit('Access Denied!');}
+
+$_CACHE['position'] = array (
+  1 => 
+  array (
+    'id' => '1',
+    'name' => '研发工程师',
+    'content' => NULL,
+    'father' => '0',
+    'date' => '2017-02-23 09:57:54',
+  ),
+  2 => 
+  array (
+    'id' => '2',
+    'name' => '销售员',
+    'content' => NULL,
+    'father' => '0',
+    'date' => '2017-02-23 09:57:54',
+  ),
+  3 => 
+  array (
+    'id' => '3',
+    'name' => '办事员',
+    'content' => NULL,
+    'father' => '0',
+    'date' => '2017-02-23 09:57:54',
+  ),
+);
+
+?>
